bdba5543f9bfdd2d123773591c76b63be96ff080
Antônio Hideto Borges Kotsubo <antoniokotsubo@gmail.com>
2025-10-04 17:21:12 -0300

    finish home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { FireGlobePage } from './pages/FireGlobePage';
+import { HomePage } from './pages/HomePage';
 
 /**
  * App Component
@@ -6,7 +7,7 @@ import { FireGlobePage } from './pages/FireGlobePage';
  */
 
 function App() {
-  return <FireGlobePage />;
+  return <HomePage />;
 }
 
 export default App;
---
+++ src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+export const useIntersectionObserver = (ref: React.RefObject<HTMLElement>, threshold = 0.5) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      {
+        threshold,
+      }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [ref, threshold]);
+
+  return isVisible;
+};
